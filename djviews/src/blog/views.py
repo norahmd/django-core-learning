a09ec0e64ba684450649190e31f59d484370d372
norahmd <435200095@student.ksu.edu.sa>
2019-12-01 11:52:44 +0300

    user context & login required decorator

--- djviews/src/blog/views.py
+++ djviews/src/blog/views.py
@@ -1,15 +1,31 @@
+from django.contrib.auth.decorators import login_required 
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import PostModel
 # Create your views here.
 
 # CRUD
-
 def post_model_list_view(request):
     qs = PostModel.objects.all()
-    print(qs)
-    template = "blog/list-view.html"
     context ={
-        "object_list": qs
+    "object_list": qs
     }
+    template = "blog/list-view.html"
+
+    return render(request, template, context)
+
+
+@login_required(login_url='/login/')
+def login_required_view(request):
+    print(request.user)
+    qs = PostModel.objects.all()
+    context ={
+    "object_list": qs
+    }
+
+    if request.user.is_authenticated:
+        template = "blog/list-view.html"
+    else:
+        template = "blog/list-view-public.html"
+
     return render(request, template, context)
