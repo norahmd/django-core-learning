4518e0700098e669916abdffad6a85f4f1ee1e8d
norahmd <435200095@student.ksu.edu.sa>
2019-12-02 10:53:09 +0300

    Update view

--- djviews/src/blog/views.py
+++ djviews/src/blog/views.py
@@ -31,6 +31,23 @@ def post_model_create_view(request):
     template = "blog/create-view.html"
     return render(request, template, context)
 
+def post_model_update_view(request, id=None):
+    obj = get_object_or_404(PostModel, id=id)
+    form = PostModelForm(request.POST or None, instance=obj)
+    context = {
+        "object": obj,
+        "form": form
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        messages.success(request, "Update blog post!")
+        return HttpResponseRedirect("/blog/{num}".format(num=obj.id))
+   
+    template = "blog/update-view.html"
+    return render(request, template, context)
+
+
 def post_model_detail_view(request, id=None):
     print("norah hello there")
     obj = get_object_or_404(PostModel, id=id)
@@ -39,6 +56,7 @@ def post_model_detail_view(request, id=None):
     }
     template = "blog/detail-view.html"
     return render(request, template, context)
+
 
 def post_model_list_view(request):
     qs = PostModel.objects.all()
